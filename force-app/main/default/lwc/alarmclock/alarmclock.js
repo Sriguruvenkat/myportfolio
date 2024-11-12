9c74d5b09ef0ac95f227191ff9e0949250fb228f
Sriguruvenkat S <sriguruvenkat@gmail.com>
2024-11-12 18:52:52 +0530

    alarm clock project

--- force-app/main/default/lwc/alarmclock/alarmclock.js
+++ force-app/main/default/lwc/alarmclock/alarmclock.js
@@ -2,9 +2,25 @@ import { LightningElement } from 'lwc';
 import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets';
 export default class Alarmclock extends LightningElement {
     clockImage=AlarmClockAssets+'/AlarmClockAssets/clock.png';
+    ringtone=new Audio(AlarmClockAssets+'/AlarmClockAssets/Clocksound.mp3');
     currentTime='';
     hours=[];
     minutes=[];
+    meridiems=['AM','PM'];
+
+    hourSelected;
+    minSelected;
+    meridiemSelected;
+    alarmTime; isAlarmSet=false; isAlarmTriggered=false;
+
+    get isFieldNotSelected(){
+      return !(this.hourSelected && this.minSelected && this.meridiemSelected);
+    }
+
+    get shakeImage(){
+      return this.isAlarmTriggered ? 'shake':'';  
+    }
+
     connectedCallback(){
         this.createHouroptions();
         this.createMinuteoptions();
@@ -33,21 +49,56 @@ export default class Alarmclock extends LightningElement {
           sec  = sec<10 ? "0"+sec : sec;
       
           this.currentTime = `${hour}:${min}:${sec} ${ampm}`;
+
+          if(this.alarmTime === `${hour}:${min}:${ampm}`){
+            this.isAlarmTriggered=true;
+            this.ringtone.play();
+            this.ringtone.loop=true;
+          }
     
         }, 1000)
     }
 
-    createHouroptions(){
+    createHouroptions(){   //dynamic hour options in the box
         for(let i=1;i<=12;i++){
             let val=i<10  ? "0"+i : i ;
             this.hours.push(val);
         }
     }
 
-    createMinuteoptions(){
+    createMinuteoptions(){   //dynamic minutes options in the box
         for(let i=0;i<=59;i++){
             let val=i<10  ? "0"+i : i ;
             this.minutes.push(val);
         }
     }
-}   
+
+    optionhandler(event){      //handles the hour value,minute value and meridiem value sent from the child dropdown component
+       const{label,value}=event.detail;
+       if(label==="Hour(s)"){
+          this.hourSelected=value;
+       }
+       else if(label==="Minute(s)"){
+          this.minSelected=value;
+       }
+       else if(label==="AM/PM"){
+          this.meridiemSelected=value;
+       }
+    }
+
+    setAlarmHandler(){     //setting the alarm time
+      this.alarmTime=`${this.hourSelected}:${this.minSelected}:${this.meridiemSelected}`
+      this.isAlarmSet=true;
+    }
+
+    clearAlarmHandler(){   //clearing the alarm time
+      this.isAlarmSet=false;
+      this.alarmTime='';
+      this.isAlarmTriggered=false;
+      this.ringtone.pause();
+      const elements=this.template.querySelectorAll('c-clock-dropdown');
+      Array.from(elements).forEach(element=>{
+        element.reset(" ");
+      })
+    }
+}     
